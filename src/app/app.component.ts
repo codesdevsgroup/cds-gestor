181d50a975269b0c20412d306ecb159a8873eef7
Lucas Campos <bhlucascampos@gmail.com>
2024-06-01 13:35:28 -0300

    ajust start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from "@angular/common";
+import {HomeComponent} from "./views/pages/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet, HomeComponent],
+  template: `
+    <router-outlet></router-outlet>
+    <app-home />
+  `,
 })
 export class AppComponent {
   title = 'cds-gestor';
